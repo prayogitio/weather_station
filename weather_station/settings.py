eec0ba9a47fb984e492148aa8f990e07e4da5963
unknown <tio.qynxx@gmail.com>
2018-03-03 13:54:58 +0700

    test

--- weather_station/settings.py
+++ weather_station/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join((BASE_DIR), 'static')
+STATIC_ROOT = os.path.join((BASE_DIR), 'assets')
 STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 
