bba84b33aa98c20a2e3618a59d347fab38eef70a
unknown <tio.qynxx@gmail.com>
2018-03-03 13:12:38 +0700

    modify station views

--- station/views.py
+++ station/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 from .models import Reading
+from .worker import fetch_data
 
 # Create your views here.
 def report(request):
+    fetch_data()
     data = Reading.objects.last()
     return render(request, 'station/station.html', { 'data': data })
 
